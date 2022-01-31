ac25e0826fd8d2239d03f417221b863a902644a1
Erico Andre <ericoandresilva@gmail.com>
2022-01-31 13:02:04 -0300

    Update saida.sh

--- saida.sh
+++ saida.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+pacman -Syy && pacman -S --noconfirm dialog terminus-font reflector
+
 ##################################################
 #                   Variaveis                    #
 ##################################################
 
+VERSION="Arch Linux Installer"
+ARCHI=$(uname -m)
+SYSTEM="Unknown"
+
 LANGUAGE=pt_BR.UTF-8
 KEYBOARD_LAYOUT=br-abnt2
+FONT=lat0-16
 
 HD=/dev/sda
 
@@ -134,14 +141,34 @@ reboote(){
   fi
 }
 ##################################################
-#                   Script                       #
+#                     GUI                        #
 ##################################################
 
-pacman -Syy && pacman -S --noconfirm dialog terminus-font reflector
+LANGUAGE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir a Localização do Sistema " --radiolist "A localização (locale) determina o idioma a ser exibido, os formatos de data e hora, etc...\n\nO formato é idioma_PAÍS (ex.: en_US é inglês, Estados Unidos; pt_PT é português, Portugal)." 15 30 4 $(cat /etc/locale.gen | grep -v "#  " | sed 's/#//g' | sed 's/ UTF-8//g' | grep .UTF-8 | sort | awk '{ print $0 "\"\"  OFF " }') --stdout)
+
+# Set the installed system's hostname
+HNAME=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Nome da Máquina " --inputbox "\nO hostname é usado para identificar o sistema em uma rede.\n\nE é restrito aos caracteres alfa numéricos, pode conter um hífen (-) - mas não no inicio ou no fim - e não deve ser maior que 63 caracteres.\n" 10 25 --stdout)
+
+# Set Zone and Sub-Zone
+ZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nO fuso horário é usado para definir correctamente o relógio do sistema." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "/" | sed "s/\/.*//g" | sort -ud | sort | awk '{ printf "\0"$0"\0"  " . " }') --stdout)
+SUBZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nSeleccione a cidade mais próxima de você." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "$ZONE/" | sed "s/$ZONE\///g" | sort -ud | sort | awk '{ printf "\0"$0"\0"  " . " }') --stdout)
+
+CLOCK=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --radiolist "\nUTC é o padrão de tempo universal e é recomendado a menos que tenha dual-boot com o Windows." 10 30 4 "utc" "" ON "localtime" "" OFF --stdout)
+
+# Definir Senha Root
+ROOT_PASSWD=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Senha ROOT " --inputbox "\nDigite a senha Root \n\n" 10 25 --stdout)
+
+# Criar Novo Usuário
+USER=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Criar Novo Usuário " --inputbox "\nDigite o nome do usuário. As letras DEVEM ser minúsculas .\n" 10 25 --stdout)
+USER_PASSWD=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Adicionar Novo Usuário " --inputbox "\nInsira a senha para $USER" 10 25 --stdout)
+
+##################################################
+#                Configuracoes                   #
+##################################################
 
 timedatectl set-ntp true
-loadkeys br-abnt2
-setfont ter-v22b
+[[ $FONT != "" ]] && setfont $FONT
+loadkeys $KEYBOARD_LAYOUT   # br-abnt2
 
 #### Particionamento esta configurado para usar todo o hd
 particionar_discos
@@ -153,22 +180,8 @@ inst_base
 inst_boot_load
 
 #### Configuracao
-arch_chroot "loadkeys br-abnt2"
+arch_chroot "loadkeys $KEYBOARD_LAYOUT"
 arch_chroot "timedatectl set-ntp true"
-
-HNAME=$(dialog  --clear --inputbox "Digite o nome do Computador" 10 25 --stdout)
-
-ZONE=$(dialog  --clear --menu "Select Sua country/zone." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "/" | sed "s/\/.*//g" | sort -ud | sort | awk '{ printf "\0"$0"\0"  " . " }') --stdout)
-SUBZONE=$(dialog  --clear --menu "Select Sua country/zone." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "$ZONE/" | sed "s/$ZONE\///g" | sort -ud | sort | awk '{ printf "\0"$0"\0"  " . " }') --stdout)
-
-LANGUAGE=$(dialog  --clear --radiolist "Escolha idioma do sistema:" 15 30 4 $(cat /etc/locale.gen | grep -v "#  " | sed 's/#//g' | sed 's/ UTF-8//g' | grep .UTF-8 | sort | awk '{ print $0 "\"\"  OFF " }') --stdout)
-CLOCK=$(dialog  --clear --radiolist "Configurcao do relojo" 10 30 4 "utc" "" ON "localtime" "" OFF --stdout)
-
-ROOT_PASSWD=$(dialog --clear --inputbox "Digite a senha de root" 10 25 --stdout)
-
-USER=$(dialog  --clear --inputbox "Digite o nome do novo Usuario" 10 25 --stdout)
-USER_PASSWD=$(dialog --clear --inputbox "Digite a senha  de $USER" 10 25 --stdout)
-
 
 #### configure base system
 #### setting hostname
@@ -181,8 +194,8 @@ arch_chroot "locale-gen"
 arch_chroot "echo -e LANG=$LANGUAGE'\n'LC_MESSAGES=$LANGUAGE> /etc/locale.conf"
 arch_chroot "export LANG=$LANGUAGE"
 
-#### Vconsole
-arch_chroot "echo -e KEYMAP=$KEYBOARD_LAYOUT'\n'FONT=lat0-16'\n'FONT_MAP= > /etc/vconsole.conf"
+#### virtual console keymap
+arch_chroot "echo -e KEYMAP=$KEYBOARD_LAYOUT'\n'FONT=$FONT'\n'FONT_MAP= > /etc/vconsole.conf"
 
 #### Setting timezone
 arch_chroot "ln -s /usr/share/zoneinfo/$ZONE/$SUBZONE /etc/localtime"
@@ -205,6 +218,8 @@ arch_chroot "sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoer
 
 #### networkmanager acpi
 arch_chroot "systemctl enable NetworkManager.service acpid.service ntpd.service"
+
+
 
 #### auto-install VM drivers
 case $(systemd-detect-virt) in
