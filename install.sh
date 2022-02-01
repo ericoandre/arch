8f3000e65c61e1893a3fdf0a7ddb15ff0e9d2d6b
Erico Andre <ericoandresilva@gmail.com>
2022-02-01 20:06:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -261,11 +261,11 @@ install_bootloader
 CURR_LOCALE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir a Localização do Sistema " --menu "A localização (locale) determina o idioma a ser exibido, os formatos de data e hora, etc...\n\nO formato é idioma_PAÍS (ex.: en_US é inglês, Estados Unidos; pt_PT é português, Portugal)." 0 0 12 $(cat /etc/locale.gen | grep -v "#  " | sed 's/#//g' | sed 's/ UTF-8//g' | grep .UTF-8 | sort | awk '{ printf $0 " - " }') --stdout)
 
 # Set the installed system's hostname
-HNAME=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Nome da Máquina " --inputbox "\nO hostname é usado para identificar o sistema em uma rede.\n\nE é restrito aos caracteres alfa numéricos, pode conter um hífen (-) - mas não no inicio ou no fim - e não deve ser maior que 63 caracteres.\n" 10 50--stdout)
+HNAME=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Nome da Máquina " --inputbox "\nO hostname é usado para identificar o sistema em uma rede.\n\nE é restrito aos caracteres alfa numéricos, pode conter um hifen (-) - mas não no inicio ou no fim - e não deve ser maior que 63 caracteres.\n" 15 60 --stdout)
 
 # Set Zone and Sub-Zone
-ZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nO fuso horário é usado para definir correctamente o relógio do sistema." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "/" | sed "s/\/.*//g" | sort -ud | sort | awk '{ printf ""$0""  " - " }') --stdout)
-SUBZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nSeleccione a cidade mais próxima de você." 20 35 15 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "$ZONE/" | sed "s/$ZONE\///g" | sort -ud | sort | awk '{ printf ""$0""  " - " }') --stdout)
+ZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nO fuso horário é usado para definir correctamente o relógio do sistema." 20 50 50 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "/" | sed "s/\/.*//g" | sort -ud | sort | awk '{ printf ""$0""  " - " }') --stdout)
+SUBZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --menu "\nSeleccione a cidade mais próxima de você." 20 50 50 $(cat /usr/share/zoneinfo/zone.tab | awk '{print $3}' | grep "$ZONE/" | sed "s/$ZONE\///g" | sort -ud | sort | awk '{ printf ""$0""  " - " }') --stdout)
 
 CLOCK=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --radiolist "\nUTC é o padrão de tempo universal e é recomendado a menos que tenha dual-boot com o Windows." 10 30 4 "utc" "" ON "localtime" "" OFF --stdout)
 
@@ -284,26 +284,26 @@ install_driver_videos
 fi
 
 #### setting hostname
-echo ${HNAME} > ${MOUNTPOINT}/etc/hostname
-echo -e "127.0.0.1    localhost.localdomain    localhost\n::1        localhost.localdomain    localhost\n127.0.1.1    ${HNAME}.localdomain    ${HNAME}" >> ${MOUNTPOINT}/etc/hosts
+echo $HNAME > $MOUNTPOINT/etc/hostname
+echo -e "127.0.0.1    localhost.localdomain    localhost\n::1        localhost.localdomain    localhost\n127.0.1.1    $HNAME.localdomain    $HNAME" >> $MOUNTPOINT/etc/hosts
 
 #### locales setting locale pt_BR.UTF-8 UTF-8
-echo -e "LANG=${LOCALE}\nLC_MESSAGES=${LOCALE}" > ${MOUNTPOINT}/etc/locale.conf
-sed -i "s/#${LOCALE}/${LOCALE}/" ${MOUNTPOINT}/etc/locale.gen
+echo -e "LANG=$LOCALE\nLC_MESSAGES=$LOCALE" > $MOUNTPOINT/etc/locale.conf
+sed -i "s/#$LOCALE/$LOCALE/" $MOUNTPOINT/etc/locale.gen
 arch_chroot "locale-gen"
-arch_chroot "export LANG=${LOCALE}"
+arch_chroot "export LANG=$LOCALE"
 
 #### virtual console keymap
-echo -e "KEYMAP=${KEYMAP}\nFONT=${FONT}" > ${MOUNTPOINT}/etc/vconsole.conf
+echo -e "KEYMAP=$KEYMAP\nFONT=$FONT" > $MOUNTPOINT/etc/vconsole.conf
 
 #### Setting timezone
-arch_chroot "ln -s /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime"
+arch_chroot "ln -s /usr/share/zoneinfo/$ZONE/$SUBZONE /etc/localtime"
 
 #### enable multilib
-arch_chroot "sed -i '/multilib\]/,+1 s/^#//' /etc/pacman.conf"
+sed -i '/multilib\]/,+1 s/^#//' $MOUNTPOINT/etc/pacman.conf
 
 #### Setting hw CLOCK
-arch_chroot "hwclock --systohc --${CLOCK}"
+arch_chroot "hwclock --systohc --$CLOCK"
 
 #### root password
 arch_chroot "echo -e $ROOT_PASSWD'\n'$ROOT_PASSWD | passwd"
