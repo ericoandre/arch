717699e4b8b843cf58f342e81b448befa0754c48
ericoandre <ericoandresilva@gmail.com>
2022-02-02 11:46:50 -0300

    .

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # Arch Linux
 
 
-### Grub Theme
-
+### Visual Code Studio
 ```bash
-mkdir -p "/boot/grub/themes/CyberRe"
-cp -a arch/CyberRe /boot/grub/themes/CyberRe
-cp -an /etc/default/grub /etc/default/grub.bak
-
-grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null
-sed -i '/GRUB_THEME=/d' /etc/default/grub
-echo "GRUB_THEME=\"/boot/grub/themes/CyberRe/theme.txt\"" >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+wget "https://go.microsoft.com/fwlink/?LinkID=620885" -O vscode.tar.gz
+sudo tar -vzxf vscode.tar.gz -C /opt/
+sudo ln -sf /opt/VSCode-linux-x64/code /usr/bin/code
+sudo echo -e '[Desktop Entry]\nVersion=1.0\nName=vscode\nExec=code\nIcon=/opt/VSCode-linux-x64/resources/app/resources/linux/code.png\nType=Application\nCategories=Development;Application' | sudo tee /usr/share/applications/vscode.desktop
 ```
+
+### Sublime Text
+```bash
+wget https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2 -O sublime_text.tar.bz2
+sudo tar xjf sublime_text.tar.bz2 -C /opt/
+sudo ln -sf /opt/sublime_text_3/sublime_text /usr/bin/sublime
+sudo echo -e '[Desktop Entry]\nVersion=1.0\nName=sublime text 3\nExec=sublime\nIcon=/opt/sublime_text_3/Icon/256x256/sublime-text.png\nType=Application\nCategories=Development;Application' | sudo tee /usr/share/applications/sublime_text.desktop
+```
+
+
+### Eclipse
+```bash
+wget https://eclipse.mirror.rafal.ca/technology/epp/downloads/release/2020-12/R/eclipse-java-2020-12-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+sudo tar -vzxf eclipse.tar.gz -C /opt/
+sudo ln -sf /opt/eclipse/eclipse /usr/bin/eclipse
+sudo echo -e '[Desktop Entry]\nVersion=1.0\nName=Eclipse\nExec=eclipse\nTerminal=false\nIcon=/opt/eclipse/icon.xpm\nType=Application\nComment=Integrated Development Environment\nCategories=Development;Application;IDE' | sudo tee /usr/share/applications/eclipse.desktop
+```
+
 
 ### Yay
 
@@ -45,6 +58,19 @@ chsh -s /usr/bin/zsh
 
 ```bash
 sudo pacman -S --noconfirm wine lib324-vkd3d lib324-libldap wine-mono wine-gecko winetricks
+```
+
+### Grub Theme
+
+```bash
+mkdir -p "/boot/grub/themes/CyberRe"
+cp -a arch/CyberRe /boot/grub/themes/CyberRe
+cp -an /etc/default/grub /etc/default/grub.bak
+
+grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null
+sed -i '/GRUB_THEME=/d' /etc/default/grub
+echo "GRUB_THEME=\"/boot/grub/themes/CyberRe/theme.txt\"" >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
 ```
 
 <!-- 
@@ -91,7 +117,33 @@ yad
 libpulse
 
 
- -->
+
+
+    arch_chroot "pacman -S --noconfirm xorg xorg-server xorg-twm xorg-xclock xorg-xinit xterm xorg-fonts-100dpi xorg-fonts-75dpi alsa-firmware alsa-utils"
+
+    case $desktop in
+        1)
+            DEpkg="gdm gnome-shell gnome-backgrounds gnome-control-center gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool nautilus gedit gnome-calculator gnome-disk-utility eog evince"
+            ;;
+        2)
+            DEpkg="gdm gnome gnome-tweak-tool"
+            ;;
+        3)
+            DEpkg="sddm plasma plasma-wayland-session dolphin konsole kate kcalc ark gwenview spectacle okular packagekit-qt5"
+            ;;
+        4)
+            DEpkg="lxdm xfce4 xfce4-goodies network-manager-applet"
+            ;;
+    esac
+
+
+
+
+
+
+
+
+-->
 
 <!-- pacman -S gdm gnome-shell gnome-control-center gnome-tweak-tool -->
 <!-- pacmam -S nautilus chromium -->
--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ CLOCK=utc
 HNAME=ArchVM
 
 # Installation
-MOUNTPOINT=/mnt
 HD=/dev/sda
 
 SWAP_SIZE=1024
@@ -60,7 +59,7 @@ fi
 ######################################################################
 
 arch_chroot() {
-    arch-chroot $MOUNTPOINT /bin/bash -c "${1}"
+    arch-chroot /mnt /bin/bash -c "${1}"
 }  
 Parted() {
     parted --script $HD "${1}"
@@ -86,44 +85,34 @@ automatic_particao() {
       # Cria partição swap
       parted -s $HD mkpart primary linux-swap $SWAP_START $SWAP_END
       Parted "mkpart primary $ROOT_FS $ROOT_START -$ROOT_END"
-      MOUNTPOINTROOT=3
-    else
-      Parted "mkpart primary $ROOT_FS $BOOT_END -$ROOT_END"
-      MOUNTPOINTROOT=2
-    fi
-    
-    # Formatando partição root
-    mkfs.$ROOT_FS ${HD}${MOUNTPOINTROOT} -L Root
-    mount ${HD}${MOUNTPOINTROOT} $MOUNTPOINT
 
-    if [[ $MOUNTPOINTROOT -eq 2 ]]; then
-      mkdir -p  $MOUNTPOINT/opt/swap && touch $MOUNTPOINT/opt/swap/swapfile
-      dd if=/dev/zero of=$MOUNTPOINT/opt/swap/swapfile bs=1M count=$SWAP_SIZE status=progress
-      chmod 600 $MOUNTPOINT/opt/swap/swapfile
-      mkswap $MOUNTPOINT/opt/swap/swapfile
-      swapon $MOUNTPOINT/opt/swap/swapfile
-    else
       mkswap ${HD}2
       swapon ${HD}2
+
+      # Formatando partição root
+      mkfs.$ROOT_FS ${HD}3-L Root
+      mount ${HD}3 /mnt
+    else
+      Parted "mkpart primary $ROOT_FS $BOOT_END -${ROOT_END}"
+
+      # Formatando partição root
+      mkfs.$ROOT_FS ${HD}2 -L Root
+      mount ${HD}2 /mnt
+
+      mkdir -p  /mnt/opt/swap && touch /mnt/opt/swap/swapfile
+      dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=$SWAP_SIZE status=progress
+      chmod 600 /mnt/opt/swap/swapfile
+      mkswap /mnt/opt/swap/swapfile
+      swapon /mnt/opt/swap/swapfile
     fi
+    
     
     if [[ "$SYSTEM" -eq "UEFI" ]]; then
         # Monta partição esp
-        mkdir -p $MOUNTPOINT/boot/efi && mount $HD1 $MOUNTPOINT/boot/efi
+        mkdir -p /mnt/boot/efi && mount ${HD}1 /mnt/boot/efi
     else
         # Monta partição boot
-        mkdir -p $MOUNTPOINT/boot && mount $HD1 $MOUNTPOINT/boot
-    fi
-}
-
-reboote(){
-    dialog --clear --title " Installation finished sucessfully " --yesno "\nDo you want to reboot?" 7 62
-    if [[ $? -eq 0 ]]; then
-        echo "System will reboot in a moment..."
-        sleep 3
-        clear
-        umount -R $MOUNTPOINT
-        reboot
+        mkdir -p /mnt/boot && mount ${HD}1 /mnt/boot
     fi
 }
 
@@ -144,11 +133,12 @@ install_driver() {
           arch_chroot "systemctl enable vboxservice.service"
       ;;
     esac
-    pacstrap ${MOUNTPOINT} xf86-input-synaptics synaptic
+    pacstrap /mnt xf86-input-synaptics synaptic
 }
 
 install_driver_videos() {
-    if grep -E "NVIDIA|GeForce" <<< $(lspci); then
+    gpu_type=$(lspci)
+    if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
         arch_chroot "pacman -S nvidia --noconfirm --needed"
         arch_chroot "nvidia-xconfig"
     elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
@@ -162,57 +152,57 @@ install_driver_videos() {
 
 install_descktopmanager() {
     install_driver_videos
+
     arch_chroot "pacman -Sy xorg xorg-xkbcomp xorg-xinit xorg-server xorg-twm xorg-xclock xorg-xinit xorg-drivers xorg-xkill xorg-fonts-100dpi xorg-fonts-75dpi mesa xterm --noconfirm --needed"
-    desktop=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --menu "Desktop Environment" 15 50 50  1 "Gnome Minimal" 2 "Gnome" 3 "Plasma kde" 4 "cinnamon" 5 "xfce4" 6 "deepin" 7 "LXQt" 8 "Minimal"  --stdout)
+
+    desktop=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "  " --menu "Desktop Environment" 15 50 50  1 "Gnome Minimal" 2 "Gnome" 3 "Plasma kde" 4 "cinnamon" 5 "xfce4" 6 "deepin" 7 "LXQt" 8 "Minimal"  --stdout)
     case $desktop in
         1)
-          DEpkg="gdm gnome-shell gnome-backgrounds gnome-control-center gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool nautilus gedit gvfs gnome-calculator gnome-disk-utility"
-          DMANAGER=1
+          DEpkg="gnome-shell gnome-backgrounds gnome-control-center gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool nautilus gedit gvfs gnome-calculator gnome-disk-utility"
           ;;
         2)
-          DEpkg="gdm gnome gnome-tweak-tool "
-          DMANAGER=1
+          DEpkg="gnome gnome-tweak-tool "
           ;;
         3)
-          DEpkg="sddm plasma plasma-wayland-session dolphin konsole kate kcalc ark gwenview spectacle okular packagekit-qt5 "
-          DMANAGER=2
+          DEpkg="plasma plasma-wayland-session dolphin konsole kate kcalc ark gwenview spectacle okular packagekit-qt5 "
           ;;
         4)
-          DEpkg="gdm cinnamon sakura gnome-disk-utility nemo-fileroller mousepad gnome-software gnome-system-monitor gnome-screenshot network-manager-applet "
-          DMANAGER=1
+          DEpkg="cinnamon sakura gnome-disk-utility nemo-fileroller mousepad gnome-software gnome-system-monitor gnome-screenshot network-manager-applet "
           ;;
         5)
-          DEpkg="lxdm xfce4 xfce4-goodies network-manager-applet file-roller leafpad "
-          DMANAGER=3
+          DEpkg="xfce4 xfce4-goodies network-manager-applet file-roller leafpad "
           ;;
         6)
-          DEpkg="lightdm lightdm-webkit2-greeter accountsservice deepin deepin-extra ark gnome-disk-utility gedit "
-          DMANAGER=4
+          DEpkg="deepin deepin-extra ark gnome-disk-utility gedit "
           ;;
         7)
-          DEpkg="lxdm lxqt xdg-utils libpulse libstatgrab libsysstat lm_sensors network-manager-applet pavucontrol-qt "
-          DMANAGER=3
+          DEpkg="lxqt xdg-utils libpulse libstatgrab libsysstat lm_sensors network-manager-applet pavucontrol-qt "
           ;;
     esac
     arch_chroot "pacman -Sy $DEpkg audacious pulseaudio pulseaudio-alsa pavucontrol xscreensaver vlc archlinux-wallpaper libreoffice-fresh tilix mesa eog gparted xdg-user-dirs-gtk firefox evince adwaita-icon-theme papirus-icon-theme oxygen-icons faenza-icon-theme --noconfirm --needed"
+
+    DMANAGER=$(dialog  --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Dm " --menu  "Qual gerenciador de exibição você gostaria de usar?" 12 50 50 1 gdm 2 lightdm 3 lxdm 4 sddm --stdout )
     case $DMANAGER in
         1)
+          arch_chroot "pacman -Sy gdm"
           arch_chroot "systemctl enable gdm.service"
           ;;
         2)
-          arch_chroot "echo -e '[Theme]\nCurrent=breeze' >> /usr/lib/sddm/sddm.conf.d/default.conf"
-          arch-chroot "systemctl enable sddm.service"
-          ;; 
+          arch_chroot "pacman -Sy lightdm lightdm-webkit2-greeter accountsservice"
+          #wget git.io/webkit2 -O tema.tar.gz && mkdir -p /mnt/usr/share/lightdm-webkit/themes/glorious
+          #tar zxvf tema.tar.gz -C /mnt/usr/share/lightdm-webkit/themes/glorious
+          # echo "webkit_theme=glorious" >> /mnt/etc/lightdm/lightdm-webkit2-greeter.conf
+          arch-chroot "systemctl enable lightdm.service"
+          ;;
         3)
+          arch_chroot "pacman -Sy lxdm"
           arch-chroot "systemctl enable lxdm.service"
           ;;
         4)
-          wget git.io/webkit2 -O tema.tar.gz && mkdir -p $MOUNTPOINT/usr/share/lightdm-webkit/themes/glorious
-          tar zxvf tema.tar.gz -C $MOUNTPOINT/usr/share/lightdm-webkit/themes/glorious
-          echo "webkit_theme=glorious" >> $MOUNTPOINT/etc/lightdm/lightdm-webkit2-greeter.conf
-          arch-chroot "systemctl enable lightdm.service"
+          arch_chroot "pacman -Sy sddm"
+          #arch_chroot "echo -e '[Theme]\nCurrent=breeze' >> /usr/lib/sddm/sddm.conf.d/default.conf"
+          arch-chroot "systemctl enable sddm.service"
           ;;
-          
     esac
 }
 
@@ -220,25 +210,25 @@ install_root() {
     KERNEL=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)"  --title "$TITLE" --radiolist "Existem vários kernels disponíveis para o sistema.\n\nO mais comum é o atual kernel linux.\nEste kernel é o mais atualizado, oferecendo o melhor suporte de hardware.\nNo entanto, pode haver possíveis erros nesse kernel, apesar dos testes.\n\nO kernel linux-lts fornece um foco na estabilidade.\nEle é baseado em um kernel mais antigo, por isso pode não ter alguns recursos mais recentes.\n\nO kernel com proteção do linux é focado na segurança \nEle contém o Grsecurity Patchset e o PaX para aumentar a segurança. \n\nO kernel do linux-zen é o resultado de uma colaboração de hackers do kernel \npara fornecer o melhor kernel possível para os sistemas cotidianos. \n\nPor favor, selecione o kernel que você deseja instalar." 50 100 100 linux "" on linux-lts "" off linux-hardened "" off linux-zen "" off --stdout)
     reflector --verbose --protocol http --protocol https --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
     [[ "$(uname -m)" = "x86_64" ]] && sed -i '/multilib\]/,+1 s/^#//' /etc/pacman.conf 
-    pacman -Sy
-    pacstrap ${MOUNTPOINT} base base-devel ${KERNEL} ${KERNEL}-headers ${KERNEL}-firmware grub `echo $EXTRA_PKGS`
+    pacman -Sy && pacstrap /mnt base base-devel ${KERNEL} ${KERNEL}-headers ${KERNEL}-firmware grub `echo $EXTRA_PKGS`
     
-    genfstab -U -p $MOUNTPOINT >> $MOUNTPOINT/etc/fstab
-    
+    #### fstab
+    genfstab -U -p /mnt >> /mnt/etc/fstab
+
     #### networkmanager acpi
     arch_chroot "systemctl enable NetworkManager.service acpid.service ntpd.service"
-    
-    cp /etc/pacman.d/mirrorlist ${MOUNTPOINT}/etc/pacman.d/mirrorlist
-    [[ "$(uname -m)" = "x86_64" ]] && sed -i '/multilib\]/,+1 s/^#//' $MOUNTPOINT/etc/pacman.conf
-    git clone https://aur.archlinux.org/yay.git $MOUNTPOINT/tmp
+
+    cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+    [[ "$(uname -m)" = "x86_64" ]] && sed -i '/multilib\]/,+1 s/^#//' /mnt/etc/pacman.conf
+    # git clone https://aur.archlinux.org/yay.git /mnt/tmp
 }
 
 install_bootloader() {
     #### Install Bootloader
     if [ "$(grep -m1 vendor_id /proc/cpuinfo | awk '{print $3}')" = "GenuineIntel" ]; then
-        pacstrap $MOUNTPOINT intel-ucode
+        pacstrap /mnt intel-ucode
     elif [ "$proc" = "AuthenticAMD" ]; then
-        pacstrap $MOUNTPOINT amd-ucode
+        pacstrap /mnt amd-ucode
     fi
     
     # Configura ambiente ramdisk inicial
@@ -246,15 +236,46 @@ install_bootloader() {
     if [[ "$SYSTEM" -eq "UEFI"  ]]; then
         arch_chroot "pacman -S --noconfirm efibootmgr dosfstools mtools"
         arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck"
-        mkdir $MOUNTPOINT/boot/efi/EFI/boot && mkdir $MOUNTPOINT/boot/grub/locale
-        cp $MOUNTPOINT/boot/efi/EFI/grub_uefi/grubx64.efi $MOUNTPOINT/boot/efi/EFI/boot/bootx64.efi
+        mkdir /mnt/boot/efi/EFI/boot && mkdir /mnt/boot/grub/locale
+        cp /mnt/boot/efi/EFI/grub_uefi/grubx64.efi /mnt/boot/efi/EFI/boot/bootx64.efi
     else
         arch_chroot "grub-install --target=i386-pc --recheck $HD"
     fi
-    cp $MOUNTPOINT/usr/share/locale/en@quot/LC_MESSAGES/grub.mo $MOUNTPOINT/boot/grub/locale/en.mo
+    cp /mnt/usr/share/locale/en@quot/LC_MESSAGES/grub.mo /mnt/boot/grub/locale/en.mo
     arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 }
 
+root_password() {
+    rtpasswd=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Senha ROOT " --inputbox "\nDigite a senha Root \n\n" 10 50 --stdout)
+    rtpasswd2=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Senha ROOT " --inputbox "\nDigite novamente a senha Root \n\n" 10 50 --stdout)
+    
+    if [ "$rtpasswd" != "$rtpasswd2" ]; then 
+        dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Senha ROOT " --msgbox "As senhas não coincidem. Tente novamente." 10 50
+        root_password
+    else
+        ROOT_PASSWD=$(echo $rtpasswd)
+    fi
+}
+user_password() {
+    userpasswd=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Adicionar Novo Usuário " --inputbox "\nDigite a senha para $USER" 10 50 --stdout)
+    userpasswd2=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Adicionar Novo Usuário " --inputbox "\nDigite novamente a senha para $USER." 10 50 --stdout)
+    if [[ "$userpasswd" != "$userpasswd2" ]]; then 
+        dialog --title "$TITLE" --msgbox  "As senhas não coincidem. Tente novamente." 10 50
+        user_password
+    else
+        USER_PASSWD=$(echo $userpasswd)
+    fi
+}
+reboote(){
+    dialog --clear --title " Installation finished sucessfully " --yesno "\nDo you want to reboot?" 7 62
+    if [[ $? -eq 0 ]]; then
+        echo "System will reboot in a moment..."
+        sleep 3
+        clear
+        umount -R /mnt
+        reboot
+    fi
+}
 
 ######################################################################
 ##                                                                  ##
@@ -287,11 +308,11 @@ SUBZONE=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Def
 CLOCK=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Fuso horário e Relógio " --radiolist "\nUTC é o padrão de tempo universal e é recomendado a menos que tenha dual-boot com o Windows." 12 50 30 "utc" "" ON "localtime" "" OFF --stdout)
 
 # Definir Senha Root
-ROOT_PASSWD=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Definir Senha ROOT " --inputbox "\nDigite a senha Root \n\n" 10 50 --stdout)
+root_password
 
 # Criar Novo Usuário
 USER=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Criar Novo Usuário " --inputbox "\nDigite o nome do usuário. As letras DEVEM ser minúsculas.\n" 10 50 --stdout)
-USER_PASSWD=$(dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title " Adicionar Novo Usuário " --inputbox "\nInsira a senha para $USER" 10 50 --stdout)
+user_password
 
 #### configure base system
 dialog --clear --backtitle "$VERSION - $SYSTEM ($ARCHI)" --title "INTEFACE GRAFICA" --clear --yesno "\nDeseja Instalar Windows Manager ?" 7 50
@@ -301,17 +322,17 @@ if [[ $? -eq 0 ]]; then
 fi
 
 #### setting hostname
-echo $HNAME > $MOUNTPOINT/etc/hostname
-echo -e "127.0.0.1    localhost.localdomain    localhost\n::1        localhost.localdomain    localhost\n127.0.1.1    $HNAME.localdomain    $HNAME" >> $MOUNTPOINT/etc/hosts
+echo $HNAME > /mnt/etc/hostname
+echo -e "127.0.0.1    localhost.localdomain    localhost\n::1        localhost.localdomain    localhost\n127.0.1.1    $HNAME.localdomain    $HNAME" >> /mnt/etc/hosts
 
 #### locales setting locale pt_BR.UTF-8 UTF-8
-echo -e "LANG=$LOCALE\nLC_MESSAGES=$LOCALE" > $MOUNTPOINT/etc/locale.conf
-sed -i "s/#$LOCALE/$LOCALE/" $MOUNTPOINT/etc/locale.gen
+echo -e "LANG=$LOCALE\nLC_MESSAGES=$LOCALE" > /mnt/etc/locale.conf
+sed -i "s/#$LOCALE/$LOCALE/" /mnt/etc/locale.gen
 arch_chroot "locale-gen"
 arch_chroot "export LANG=$LOCALE"
 
 #### virtual console keymap
-echo -e "KEYMAP=$KEYMAP\nFONT=$FONT" > $MOUNTPOINT/etc/vconsole.conf
+echo -e "KEYMAP=$KEYMAP\nFONT=$FONT" > /mnt/etc/vconsole.conf
 
 #### Setting timezone
 arch_chroot "ln -s /usr/share/zoneinfo/$ZONE/$SUBZONE /etc/localtime"
